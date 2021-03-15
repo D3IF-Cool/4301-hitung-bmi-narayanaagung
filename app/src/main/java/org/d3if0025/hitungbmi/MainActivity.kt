cdcec0f34091c4d68c9776722da1410b5eb4b199
Agung Narayana Adhi Putra <narayanaagung@student.telkomuniversity.ac.id>
2021-03-15 11:00:22 +0700

    step 4 nav controller

--- app/src/main/java/org/d3if0025/hitungbmi/MainActivity.kt
+++ app/src/main/java/org/d3if0025/hitungbmi/MainActivity.kt
@@ -5,11 +5,22 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.widget.Toast
-import org.d3if0025.hitungbmi.databinding.ActivityMainBinding
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
 //challenge dikerjakan
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
